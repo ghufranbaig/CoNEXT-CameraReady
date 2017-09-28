5cfe326ac9bd44f7db193966749cc494280d0442
Ghufran Baig <ghufran@cs.utexas.edu>
2017-09-28 15:43:56 -0500

    init

---
+++ figs/export_fig.m
@@ -0,0 +1,11 @@
+function export_fig(h,outfilename)
+
+set(h, 'PaperUnits','centimeters');
+set(h, 'Units','centimeters');
+pos=get(h,'Position');
+set(h, 'PaperSize', [pos(3) pos(4)]);
+set(h, 'PaperPositionMode', 'manual');
+set(h, 'PaperPosition',[0 0 pos(3) pos(4)]);
+saveas(gcf,outfilename,'pdf')
+%print('-dpdf',outfilename);
+end
